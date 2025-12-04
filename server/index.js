675c7cab76e325442e7ef65a949552e61cf54ea0
Sahasi52 <137809801+Sahasi52@users.noreply.github.com>
2025-12-04 22:37:46 +0600

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import authRouter from "./routes/authRoutes.js";
+import { connectToDatabase } from "./lib/db.js";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,6 +13,19 @@ app.use(
 );
 app.use(express.json());
 app.use("/auth", authRouter);
+app.get("/", (req, res) => {
+  res.send("Backend is running!");
+});
+app.get("/test-db", async (req, res) => {
+  try {
+    const db = await connectToDatabase();
+    const [rows] = await db.query("SELECT 1 + 1 AS result");
+    res.json({ result: rows[0].result });
+  } catch (err) {
+    console.error("DB Error:", err.message);
+    res.status(500).json({ error: err.message });
+  }
+});
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
