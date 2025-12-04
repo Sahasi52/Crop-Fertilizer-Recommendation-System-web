b3b21617f0daa895027bf0e888020a81da4f51f3
Sahasi52 <137809801+Sahasi52@users.noreply.github.com>
2025-12-04 23:00:13 +0600

    Update db.js

--- server/lib/db.js
+++ server/lib/db.js
@@ -9,7 +9,6 @@ export const connectToDatabase = async () => {
       user: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
-      port: process.env.PORT || 3306,
     });
   }
 
